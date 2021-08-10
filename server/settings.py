80a89f7f3594aaa924389356de72a4a86daf8886
Omar Sawadogo <sawomar226@gmail.com>
2021-08-10 02:49:57 +0000

    better

--- server/settings.py
+++ server/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-wly4-70tgf%59^65b*aw+2%-sk=i&w@p=#ala(azq)^7^a6q-j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['ibam-api.herokuapp.com','server-api-ibam.herokuapp.com']
 
 
 # Application definition
