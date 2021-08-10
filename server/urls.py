67d1f64b19dba60cf9ecaa8d4a9f7a2a7296656f
Omar Sawadogo <sawomar226@gmail.com>
2021-08-10 01:50:24 +0000

    make it better

--- server/urls.py
+++ server/urls.py
@@ -21,7 +21,6 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include("api.urls")),
-    path('/', include_docs_urls(title='Cours Api', public=True)),
 
 ]
 
